c74e621ef9a72ed592970aacc20e79cdd5504ebe
Caleb Odinga <calemasanga@gmail.com>
2021-08-16 18:39:19 +0300

    adds image links to styles css, adds url conf, adds view function to projects, index and hobbies

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^$', views.index, name = 'index')
+  url(r'^$', views.index, name = 'index'),
+  url(r'^projects/$', views.projects, name = 'project'),
+  url(r'^hobbies/$', views.hobbies, name = 'hobby'),
+  url(r'^contact/$', views.contacts, name = 'contact')
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -13,3 +13,36 @@ def index(request):
   }
 
   return render(request, 'all_templates/index.html', context)
+
+#view function to projects page
+def projects(request):
+  '''
+  renders projects page
+  '''
+  title = 'Projects'
+  context = {
+    'title':title
+  }
+  return render(request, 'all_templates/projects.html', context)
+
+#view function to projects page
+def hobbies(request):
+  '''
+  renders hobbies page
+  '''
+  title = 'Fun side'
+  context = {
+    'title':title
+  }
+  return render(request, 'all_templates/hobbies.html', context)
+
+#view function to projects page
+def contacts(request):
+  '''
+  renders contacts page
+  '''
+  title = 'Contacts'
+  context = {
+    'title':title
+  }
+  return render(request, 'all_templates/index.html', context)
