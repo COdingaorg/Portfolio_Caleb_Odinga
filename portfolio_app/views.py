5cf80b47f29a7b401dd4f51b3cb76c674ff6158e
Caleb Odinga <calemasanga@gmail.com>
2021-08-17 12:12:12 +0300

    adds profile

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from portfolio_app.forms import CreateProfile, ToolsForm
+from portfolio_app.forms import BackgroundForm, ContactForm, CreateProfile, HobbyForm, ProjectsForm, SkillForm, SocialForm, ToolsForm
 from django.shortcuts import redirect, render
 from .models import Profile, User
 
@@ -42,6 +42,8 @@ def hobbies(request):
   '''
   title = 'Fun side'
   context = {
+    'user': user,
+    'profile':main_profile,
     'title':title
   }
   return render(request, 'all_templates/hobbies.html', context)
@@ -51,11 +53,26 @@ def contacts(request):
   '''
   renders contacts page
   '''
-  title = 'Contacts'
-  context = {
-    'title':title
-  }
-  return render(request, 'all_templates/contact.html', context)
+
+  if request.method == "POST" and 'update_profile' in request.POST:
+    form = ContactForm(request.POST)
+    if form.is_valid():
+      new_profile = form.save(commit = False)
+      new_profile.user = user
+      new_profile.save()
+
+      messages.success(request, 'Profile created successfully')
+      return redirect('profile')
+  else: 
+    form = ContactForm
+    title = 'Contacts'
+    context = {
+      'user': user,
+      'profile':main_profile,
+      'form':form,
+      'title':title
+    }
+    return render(request, 'all_templates/contact.html', context)
 
 #view function to profile page
 def profile(request):
@@ -87,64 +104,153 @@ def background(request):
   '''
   renders background page
   '''
-  title = 'background'
-  context = {
-    'title':title
-  }
-  return render(request, 'all_templates/background.html', context)
+  if request.method == "POST" and 'update_profile' in request.POST:
+    form = BackgroundForm(request.POST)
+    if form.is_valid():
+      new_profile = form.save(commit = False)
+      new_profile.user = user
+      new_profile.save()
+
+      messages.success(request, 'Background created successfully')
+      return redirect('profile')
+  else: 
+    form = BackgroundForm
+    title = 'background'
+    context = {
+      'user': user,
+      'profile':main_profile,
+      'form':form,
+      'title':title,
+    }
+    return render(request, 'all_templates/background.html', context)
 
 #view function to skill page
 def skills(request):
   '''
   renders skills page
   '''
-  title = 'skills'
-  context = {
-    'title':title
-  }
-  return render(request, 'all_templates/skills.html', context)
+
+  if request.method == "POST" and 'update_profile' in request.POST:
+    form = SkillForm(request.POST, request.FILES)
+    if form.is_valid():
+      new_profile = form.save(commit = False)
+      new_profile.user = user
+      new_profile.save()
+
+      messages.success(request, 'Profile created successfully')
+      return redirect('profile')
+  else: 
+    form = SkillForm
+    title = 'skills'
+    context = {
+      'user': user,
+      'profile':main_profile,
+      'form':form,
+      'title':title
+    }
+    return render(request, 'all_templates/skills.html', context)
 
 #view function to tools page
 def tools(request):
   '''
   renders tools page
   '''
-  title = 'tools'
-  context = {
-    'title':title
-  }
-  return render(request, 'all_templates/tools.html', context)
+
+  if request.method == "POST" and 'update_profile' in request.POST:
+    form = ToolsForm(request.POST, request.FILES)
+    if form.is_valid():
+      new_profile = form.save(commit = False)
+      new_profile.user = user
+      new_profile.save()
+
+      messages.success(request, 'Profile created successfully')
+      return redirect('profile')
+  else: 
+    form = ToolsForm
+    title = 'tools'
+    context = {
+      'user': user,
+      'profile':main_profile,
+      'form':form,
+      'title':title
+    }
+    return render(request, 'all_templates/tools.html', context)
 
 #view function to social page
 def social(request):
   '''
   renders social accounts page
   '''
-  title = 'social'
-  context = {
-    'title':title
-  }
-  return render(request, 'all_templates/social.html', context)
+
+  if request.method == "POST" and 'update_profile' in request.POST:
+    form = SocialForm(request.POST, request.FILES)
+    if form.is_valid():
+      new_profile = form.save(commit = False)
+      new_profile.user = user
+      new_profile.save()
+
+      messages.success(request, 'Profile created successfully')
+      return redirect('profile')
+  else: 
+    form = SocialForm
+    title = 'social'
+    context = {
+      'user': user,
+      'profile':main_profile,
+      'form':form,
+      'title':title
+    }
+    return render(request, 'all_templates/social.html', context)
 
 #view function to hobby form page
 def hobbyform(request):
   '''
   renders hobby form page
   '''
-  title = 'hobby'
-  context = {
-    'title':title
-  }
-  return render(request, 'all_templates/social.html', context)
+
+  if request.method == "POST" and 'update_profile' in request.POST:
+    form = HobbyForm(request.POST, request.FILES)
+    if form.is_valid():
+      new_profile = form.save(commit = False)
+      new_profile.user = user
+      new_profile.save()
+
+      messages.success(request, 'Profile created successfully')
+      return redirect('profile')
+  else: 
+    form = HobbyForm
+    title = 'hobby'
+    context = {
+      'user': user,
+      'profile':main_profile,
+      'form':form,
+      'title':title
+    }
+    return render(request, 'all_templates/social.html', context)
 
 #view function to project form page
 def projectform(request):
   '''
   renders project form page
   '''
-  title = 'project'
-  context = {
-    'title':title
-  }
-  return render(request, 'all_templates/social.html', context)
+
+  if request.method == "POST" and 'update_profile' in request.POST:
+    form = ProjectsForm(request.POST, request.FILES)
+    if form.is_valid():
+      new_profile = form.save(commit = False)
+      new_profile.user = user
+      new_profile.save()
+
+      messages.success(request, 'Profile created successfully')
+      return redirect('profile')
+  else: 
+    form = ProjectsForm
+    title = 'project'
+    context = {
+      'user': user,
+      'profile':main_profile,
+      'form':form,
+      'title':title
+    }
+    return render(request, 'all_templates/social.html', context)
 
