84ce894336402a599d3d94eb185a8d33e8aee2f6
Codingaorg <calemasanga@gmail.com>
2021-09-07 21:49:56 +0300

    imports and injects httpclient to services

--- portfoliofrontend/src/app/services/background.service.ts
+++ portfoliofrontend/src/app/services/background.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackgroundService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 }
